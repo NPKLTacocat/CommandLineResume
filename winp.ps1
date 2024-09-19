f4b66c1759587a530863da46c3561e97860970a6
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2024-09-19 14:00:03 -0400

    Add files via upload

---
+++ winp.ps1
@@ -0,0 +1,15 @@
+# Content of the script.ps1
+@"
+-----------------------------------
+           RESUME
+-----------------------------------
+Name: John Doe
+Title: Software Engineer
+Email: john.doe@example.com
+Phone: +123-456-7890
+
+Skills:
+- Programming: Python, PowerShell, C#
+- Tools: Git, Jenkins, Docker
+-----------------------------------
+"@
